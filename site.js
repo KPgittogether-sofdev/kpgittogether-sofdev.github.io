122b0cf5ecd9b261fa9706b0129059f60b1f8b63
KPgittogether-sofdev <800100700@fvtc.edu>
2025-10-04 14:07:39 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,15 @@
-//tester//
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+ 
+if (isMorning) {
+  console.log("Good morning! ");
+} else if (isAfternoon) {
+  console.log("Good afternoon! ");
+} else if (isEvening) {
+  console.log("Good evening! ");
+} else {
+  console.log("Hello!");
+}
