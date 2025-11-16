9ef1385f5abb8b70b7ef7a8d0de3c9795a6175ea
KPgittogether-sofdev <800100700@fvtc.edu>
2025-11-16 14:35:27 -0600

    Added carousel

--- site.js
+++ site.js
@@ -26,3 +26,43 @@ if (isMorning) {
 } else {
   console.log("Hello!");
 }
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+const prevButton = document.querySelector('#prev')
+prevButton.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+const nextButton = document.querySelector('#next')
+nextButton.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+setInterval(() => {
+    console.log();
+    currentImage++
+    showImages()
+}, 5000)
+
+showImages()
+
